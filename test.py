86baff4fa182abb685f64cf269b00b6e55a4aa8d
Lemonekousagi <lemonsquash55@gmail.com>
2025-09-19 12:27:37 +0900

    CNN

--- test.py
+++ test.py
@@ -9,44 +9,9 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
-
-# x = torch.rand(5, 3)
-
-# y = x.view(-1, 5)
-
-device = torch.device("cuda")
-
-# if torch.cuda.is_available():
-#     device = torch.device("cuda")          # a CUDA device object
-#     y = torch.ones_like(x, device=device)  # directly create a tensor on GPU
-#     x = x.to(device)                       # or just use strings ``.to("cuda")``
-#     z = x + y
-#     print(z)
-#     print(z.to("cpu", torch.double))       # ``.to`` can also change dtype together!
-
-# print(torch.__version__)
-
-
-# train_set = datasets.MNIST(
-#     root=data_root,
-#     train=True,
-#     download=True,
-# )
-
-# print(f"データ数: {len(train_set)}")
-# print(f"最初のラベル: {train_set[0][1]}")
-
-# image, label = train_set[0]
-# print('image type', type(image))
-# print('image shape', image.shape)
-
-# print('min: ', image.data.min())
-# print('max: ', image.data.max())
-
-# plt.figure(figsize=(2, 2))
-# plt.title(f'{label}')
-# plt.imshow(image, cmap = 'gray_r')
-# plt.show()
+#GPUが使えるかどうか
+if torch.cuda.is_available():
+    device = torch.device("cuda")
 
 data_root = './data'
 
@@ -54,11 +19,10 @@ data_root = './data'
 transform = transforms.Compose([ 
     transforms.ToTensor(),
     transforms.Normalize((0.5,), (0.5,)),
-    transforms.Lambda(lambda x : x.view(-1))
 ])
 
 #訓練用データ
-train_set = datasets.MNIST(
+train_set = datasets.CIFAR10(
     root=data_root,
     train=True,
     download=True,
@@ -66,7 +30,7 @@ train_set = datasets.MNIST(
 )
 
 #テスト用データ
-test_set = datasets.MNIST(
+test_set = datasets.CIFAR10(
     root=data_root,
     train=False,
     download=True,
@@ -100,32 +64,40 @@ test_loader = DataLoader(
 )
 
 #学習
-n_input = 784
+n_input = 32 * 32 * 3
 n_output = 10
 n_hidden = 128
 
 #モデルクラスの定義
-class Net(nn.Module): #Netの部分は任意だが、必ずnn.Moduleを継承
-  def __init__(self, n_input, n_output, n_hidden):
+class CNN(nn.Module): #Netの部分は任意だが、必ずnn.Moduleを継承
+  def __init__(self, n_output, n_hidden):
     super().__init__()
-    self.l1 = nn.Linear(n_input, n_hidden)
-    self.l2 = nn.Linear(n_hidden, n_output)
+    self.conv1 = nn.Conv2d(3, 32, 3) #入力チャネル数(最初はRGBなので3)、出力チャネル数、フィルタサイズ
+    self.conv2 = nn.Conv2d(32, 64, 3)
+    self.maxpool = nn.MaxPool2d((2, 2)) #プーリング層
+    self.flatten = nn.Flatten() #多次元配列を1次元に変換
+
+    self.fc1 = nn.Linear(2304, n_hidden) #全結合層
+    self.fc2 = nn.Linear(n_hidden, n_output)
+
     self.relu = nn.ReLU(inplace = True)
   
   def forward(self, x):
-    x1 = self.l1(x)
-    x2 = self.relu(x1)
-    x3 = self.l2(x2)
-    return x3
+    x1 = self.maxpool(self.relu(self.conv1(x))) #畳み込み層1
+    x2 = self.maxpool(self.relu(self.conv2(x1))) #畳み込み層2
+    x3 = self.flatten(x2) #多次元配列を1次元に変換
+    x4 = self.relu(self.fc1(x3)) #全結合層1
+    x5 = self.fc2(x4) #全結合層2
+    return x5
 
 #インスタンス化
-net = Net(n_input, n_output, n_hidden).to(device)
+net = CNN(n_output, n_hidden).to(device)
 
 #損失関数の定義
 criterion = nn.CrossEntropyLoss()
 
 #最適化(重みの更新)の設定
-lr = 0.01
+lr = 0.001
 optimizer = optim.SGD(net.parameters(), lr = lr) #確率的勾配降下法(パラメータwと学習率-η)
 
 #評価結果を記録するための配列
@@ -135,7 +107,7 @@ history
 #学習開始
 from tqdm import tqdm
 
-num_epoch = 50 #何回学習するか
+num_epoch = 3000 #何回学習するか
 
 for epoch in range(num_epoch):
     train_acc, train_loss = 0, 0
@@ -175,8 +147,8 @@ for epoch in range(num_epoch):
 
     train_acc = train_acc / n_train
     val_acc = val_acc / n_test
-    
-    tarin_loss = train_loss * batch_size / n_train
+
+    train_loss = train_loss * batch_size / n_train
     val_loss = val_loss * batch_size / n_test
 
     print(f'Epoch [{epoch + 1} / {num_epoch}], loss: {train_loss:.5f} acc: {train_acc:.5f} val_loss: {val_loss:.5f} ,val_acc: {val_acc:.5f}')
@@ -203,37 +175,46 @@ plt.title('accuracy')
 plt.legend()
 plt.show()
 
-# DataLoaderから最初の1セットを取得
-for images, labels in test_loader:
-    break
 
-#予測結果の取得
-inputs = images.to(device)
-labels = labels.to(device)
-outputs = net(inputs)
-predicted = torch.max(outputs, 1)[1]
+# #おまけ:予測結果の表示
+# #正解ラベル
+# classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+# #検証データ最初の50件を表示
+# show_images_labels(test_loader, classes, net, device)
 
-#最初の50件でイメージを「正解値:予測値」と表示
+# def show_images_labels(loader, classes, net, device):
 
-plt.figure(figsize = (10, 8))
-for i in range(50):
-    ax = plt.subplot(5, 10, i + 1)
+#     # DataLoaderから最初の1セットを取得
+#     for images, labels in loader:
+#         break
 
-    # numpyに変換
-    image = images[i]
-    label = labels[i]
-    pred = predicted[i]
-    if (pred == label):
-        c = 'k'
-    else:
-        c = 'b'
-    
-    # imgの範囲を[0, 1]に戻す
-    image2 = (image + 1)/ 2
+#     #予測結果の取得
+#     inputs = images.to(device)
+#     labels = labels.to(device)
+#     outputs = net(inputs)
+#     predicted = torch.max(outputs, 1)[1]
 
-    # イメージを表示
-    plt.imshow(image2.reshape(28, 28),cmap='gray_r')
-    ax.set_title(f'{label}:{pred}', c=c)
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
+#     #最初の50件でイメージを「正解値:予測値」と表示
+
+#     plt.figure(figsize = (10, 8))
+#     for i in range(50):
+#         ax = plt.subplot(5, 10, i + 1)
+
+#         # numpyに変換
+#         image = images[i]
+#         label = labels[i]
+#         pred = predicted[i]
+#         if (pred == label):
+#             c = 'k'
+#         else:
+#             c = 'b'
+        
+#         # imgの範囲を[0, 1]に戻す
+#         image2 = (image + 1)/ 2
+
+#         # イメージを表示
+#         plt.imshow(image2.reshape(28, 28),cmap='gray_r')
+#         ax.set_title(f'{label}:{pred}', c=c)
+#         ax.get_xaxis().set_visible(False)
+#         ax.get_yaxis().set_visible(False)
+#     plt.show()
